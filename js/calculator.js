d2a259476c1ad314cac1e639cd428fbe7809a65a
Leon Tan <101375873+itsLeonTan@users.noreply.github.com>
2026-03-12 14:56:59 -0700

    v1.6.0 - implemented calculator page functionality
    Calculator is now fully functional!

--- js/calculator.js
+++ js/calculator.js
@@ -1,9 +1,8 @@
-const xValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const yValues = [50000, 200000, 150000, 200000, 300000, 320000, 340000, 390000, 340000, 420000];
+let xValues = [];
+let yValues = [];
 
 const ctx = document.getElementById('myChart');
-
-new Chart(ctx, {
+const myChart = new Chart(ctx, {
     type: "line",
     data: {
         labels: xValues,
@@ -41,3 +40,82 @@ new Chart(ctx, {
         }
     }
 });
+
+function fillChartAndTable(xValues, yValues) {
+    // Clear Arrays
+    xValues.length = 0;
+    yValues.length = 0;
+    
+    xValues.push(0);
+    yValues.push(initial);
+    
+    let table = document.getElementById("table-breakdown");
+    let sumInvested = initial;
+    let sumInterest = 0;
+    
+    // Delete existing rows
+    numRows = table.rows.length;
+    for (let i = 1; i < numRows; i++) {
+        table.deleteRow(1);
+    }
+    
+    let tableRow_0 = table.insertRow();
+    tableRow_0.insertCell(0).textContent = 0;
+    tableRow_0.insertCell(1).textContent = "$" + initial.toLocaleString();
+    tableRow_0.insertCell(2).textContent = "$0";
+    tableRow_0.insertCell(3).textContent = "$" + initial.toLocaleString();
+
+    for (let i = 1; i <= years; i++) {
+        let yearlyContribution = contribution * 12;
+        let currentInterest = (yValues[i - 1] + yearlyContribution) * (interest / 100);
+        let currentBalance = yValues[i - 1] + yearlyContribution + currentInterest;
+        xValues.push(i);
+        yValues.push(currentBalance);
+    
+        sumInvested += yearlyContribution;
+        sumInterest += currentInterest;
+
+        let tableAddRow = table.insertRow();
+        tableAddRow.insertCell(0).textContent = i;
+        tableAddRow.insertCell(1).textContent = "$" + sumInvested.toLocaleString();
+        tableAddRow.insertCell(2).textContent = "$" + (Math.round(sumInterest * 100) / 100).toLocaleString();
+        tableAddRow.insertCell(3).textContent = "$" + (Math.round(currentBalance * 100) / 100).toLocaleString();
+    }
+    myChart.update();
+
+    document.getElementById("totalInvested").textContent = "$" + (Math.round(sumInvested * 100) / 100).toLocaleString();
+    document.getElementById("totalInterest").textContent = "$" + (Math.round(sumInterest * 100) / 100).toLocaleString();
+    document.getElementById("finalBalance").textContent = "$" + (Math.round((sumInvested + sumInterest) * 100) / 100).toLocaleString();
+}
+
+let initial = document.getElementById("initial");
+let contribution = document.getElementById("contribution");
+let interest = document.getElementById("interest");
+let years = document.getElementById("years");
+
+initial.addEventListener('input', function() {
+    initial = document.getElementById("initial");
+    initial = Number(initial.value);
+    fillChartAndTable(xValues, yValues);
+});
+contribution.addEventListener('input', function() {
+    contribution = document.getElementById("contribution");
+    contribution = Number(contribution.value);
+    fillChartAndTable(xValues, yValues);
+});
+interest.addEventListener('input', function() {
+    interest = document.getElementById("interest");
+    interest = Number(interest.value);
+    fillChartAndTable(xValues, yValues);
+});
+years.addEventListener('input', function() {
+    years = document.getElementById("years");
+    years = Number(years.value);
+    fillChartAndTable(xValues, yValues);
+});
+
+initial = Number(initial.value);
+contribution = Number(contribution.value);
+interest = Number(interest.value);
+years = Number(years.value);
+fillChartAndTable(xValues, yValues);
